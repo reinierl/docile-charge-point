101b5c5ad94fb8c59ceffe54ff62ad77ee09dc5d
Reinier Lamers <r.lamers@newmotion.com>
2017-12-24 20:42:33 +0100

    clean up my set of local scripts

--- examples/do-a-transaction.scala
+++ examples/do-a-transaction.scala
@@ -1,15 +1,16 @@
-val auth = authorize("12345678").idTag
+val chargeTokenId = "01234567"
+val auth = authorize(chargeTokenId).idTag
 
 if (auth.status == AuthorizationStatus.Accepted) {
 
   statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Preparing)))
-  val transId = startTransaction(meterStart = 300, idTag = "12345678").transactionId
+  val transId = startTransaction(meterStart = 300, idTag = chargeTokenId).transactionId
   statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Charging)))
 
   prompt("Press ENTER to stop charging")
 
   statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Finishing)))
-  stopTransaction(transactionId = transId, idTag = Some("12345678"))
+  stopTransaction(transactionId = transId, idTag = Some(chargeTokenId))
   statusNotification(status = ChargePointStatus.Available())
 
 } else {
