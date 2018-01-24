29adc588450769b0190ebabd14d474af00bfe8dc
Reinier Lamers <r.lamers@newmotion.com>
2018-01-24 15:51:20 +0100

    extend test of ReceivedMsgManager

--- src/test/scala/chargepoint/docile/dsl/ReceivedMsgManagerSpec.scala
+++ src/test/scala/chargepoint/docile/dsl/ReceivedMsgManagerSpec.scala
@@ -1,8 +1,9 @@
 package chargepoint.docile.dsl
 
 import chargepoint.docile.dsl.expectations.IncomingMessage
-import com.thenewmotion.ocpp.messages.StatusNotificationRes
+import com.thenewmotion.ocpp.messages._
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 import org.specs2.concurrent.ExecutionEnv
 
 class ReceivedMsgManagerSpec(implicit ee: ExecutionEnv) extends Specification {
@@ -31,20 +32,37 @@ class ReceivedMsgManagerSpec(implicit ee: ExecutionEnv) extends Specification {
       sut.dequeue(1) must beEqualTo(List(testMsg)).await
     }
 
-    "fulfill request for messages once enough are available" in {
+    "fulfill request for messages once enough are available" in new TestScope {
       val sut = new ReceivedMsgManager
-      val testMsg = IncomingMessage(StatusNotificationRes)
 
-      sut.enqueue(testMsg)
-      sut.enqueue(testMsg)
+      sut.enqueue(testMsg(1))
+      sut.enqueue(testMsg(2))
 
       val f = sut.dequeue(3)
 
       f.isCompleted must beFalse
 
-      sut.enqueue(testMsg)
+      sut.enqueue(testMsg(3))
 
-      f must beEqualTo(List(testMsg, testMsg, testMsg)).await
+      f must beEqualTo(List(testMsg(1), testMsg(2), testMsg(3))).await
     }
+
+    "allow a peek at what's in the queue" in new TestScope {
+      val sut = new ReceivedMsgManager
+
+      sut.enqueue(testMsg(1))
+      sut.enqueue(testMsg(2))
+
+      sut.currentQueueContents mustEqual List(testMsg(1), testMsg(2))
+    }
+  }
+
+  private trait TestScope extends Scope {
+    val testIdTagInfo = IdTagInfo(status = AuthorizationStatus.Accepted)
+
+    def testMsg(seqNo: Int) = IncomingMessage(StartTransactionRes(
+      transactionId = seqNo,
+      idTag = testIdTagInfo
+    ))
   }
 }
