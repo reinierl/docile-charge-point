0410b9b85593df1bbf617848fb98029a07a9d3d3
Reinier Lamers <r.lamers@newmotion.com>
2018-01-24 16:50:27 +0100

    make sure no parens required after q in interactive mode

--- src/main/scala/chargepoint/docile/test/InteractiveOcppTest.scala
+++ src/main/scala/chargepoint/docile/test/InteractiveOcppTest.scala
@@ -1,11 +1,15 @@
 package chargepoint.docile
 package test
 
-import dsl.{CoreOps, OcppTest}
-import dsl.{expectations, shortsend}
+import dsl._
 
 class InteractiveOcppTest extends OcppTest {
-  val ops = new CoreOps with expectations.Ops with shortsend.Ops {
+
+  private val ops = new CoreOps with expectations.Ops with shortsend.Ops {
+    def connectionData = InteractiveOcppTest.this.connectionData
+  }
+
+  private val promptCommands = new InteractiveOcppTest.PromptCommands {
     def connectionData = InteractiveOcppTest.this.connectionData
   }
 
@@ -14,6 +18,7 @@ class InteractiveOcppTest extends OcppTest {
     val imports =
       """
         |import ops._
+        |import promptCommands._
         |import scala.language.postfixOps
         |import scala.concurrent.duration._
         |import java.time._
@@ -22,13 +27,23 @@ class InteractiveOcppTest extends OcppTest {
 
     ammonite.Main(predefCode = imports).run(
       "ops" -> ops,
-      "q" -> { () =>
-        println {
-          connectionData.receivedMsgManager.currentQueueContents.mkString("\n")
-        }
-      }
+      "promptCommands" -> promptCommands
     )
 
     ()
+  }
+}
+
+object InteractiveOcppTest {
+
+  trait PromptCommands {
+
+    protected def connectionData: OcppConnectionData
+
+    def q: Unit =
+      connectionData.receivedMsgManager.currentQueueContents foreach println
+
+    def whoami: Unit =
+      println(connectionData.chargePointIdentity)
   }
 }
